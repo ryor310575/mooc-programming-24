e6c4979bfe3f938630a0d608d0c0ac564b887e21
ryor310575 <ryor310575@gmail.com>
2024-12-22 13:54:43 -0500

    Commit with range_of_list

--- part04-19_range_of_list/src/range_of_list.py
+++ part04-19_range_of_list/src/range_of_list.py
@@ -1,4 +1,20 @@
+# Please write a function named range_of_list, which takes a list of 
+# integers as an argument. The function returns the difference between 
+# the smallest and the largest value in the list.
+
+# my_list = [1, 2, 3, 4, 5]
+# result = range_of_list(my_list))
+# print("The range of the list is", result)
+# Sample output
+# The range of the list is 4
+
+
 # Write your solution here
+def range_of_list(my_list: list) -> int:
+   maximun=max(my_list)
+   minimun=min(my_list)
+   list_range = maximun - minimun
+   return list_range
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     my_list = [3, 6, -4]
